1417448ed979281155ae5e08616b42e137b4158c
SebuZZZv <110625365+SebuZZZv@users.noreply.github.com>
2022-10-04 19:17:48 +0200

    first commit

---
+++ jquery.js
@@ -0,0 +1,318 @@
+$(document).ready(function(){
+    $("#btn2").click(function(){
+      $(".blackphone-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible',
+      
+});
+    });
+    $("#openbtn1").click(function(){
+      $(".blackphone-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#closebtn2").click(function(){
+      $(".topgun-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+        overflow: 'visible'
+      
+  });
+    });
+    $("#openbtn2").click(function(){
+      $(".topgun-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#closebtn3").click(function(){
+      $(".predator-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#openbtn3").click(function(){
+      $(".predator-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#closebtn4").click(function(){
+      $(".blackwidow-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#openbtn4").click(function(){
+      $(".blackwidow-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#sclosebtn1").click(function(){
+      $(".got-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#sopenbtn1").click(function(){
+      $(".got-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#sclosebtn2").click(function(){
+      $(".shehulk-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#sopenbtn2").click(function(){
+      $(".shehulk-about").fadeToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#sclosebtn3").click(function(){
+      $(".see-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#sopenbtn3").click(function(){
+      $(".see-about").fadeToggle();
+      
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#sclosebtn4").click(function(){
+      $(".twd-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#sopenbtn4").click(function(){
+      $(".twd-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#sclosebtn5").click(function(){
+      $(".sandman-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#sopenbtn5").click(function(){
+      $(".sandman-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  //COMMING SOON AREA
+  $(document).ready(function(){
+    $("#coclosebtn1").click(function(){
+      $(".commingsoon-about").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#coopenbtn1").click(function(){
+      $(".commingsoon-about").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#coclosebtn2").click(function(){
+      $(".commingsoon-about2").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#coopenbtn2").click(function(){
+      $(".commingsoon-about2").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#coclosebtn3").click(function(){
+      $(".commingsoon-about3").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#coopenbtn3").click(function(){
+      $(".commingsoon-about3").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#coclosebtn4").click(function(){
+      $(".commingsoon-about4").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#coopenbtn4").click(function(){
+      $(".commingsoon-about4").fadeToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+       
+      
+      
+  });
+    });
+  });
+  $(document).ready(function(){
+    $("#coclosebtn5").click(function(){
+      $(".commingsoon-about5").slideToggle();
+      $('html, body').removeAttr("style")
+      $('html, body').css({
+      overflow: 'visible'
+      
+  });
+    });
+    $("#coopenbtn5").click(function(){
+      $(".commingsoon-about5").fadeToggle();
+      $('html, body').css({
+      overflow: 'hidden',
+      height: '100%',
+      
+      
+      
+  });
+    });
+  });
+;
+  $(document).ready(function() {
+    $('.video-selector').click(function() {
+      $('#main-video').attr('src', $(this).attr('data-src'));
+    });
+  });
+  ;
+  $(window).scroll(function(){
+    var top = $(window).scrollTop();
+    var find_class_small = $.contains('#nav', '.small');
+    var find_class_small = $.contains('.logo', '.small');
+    var find_class_small = $.contains('.main-navigation', '.small');
+  
+    if(top > 150 && find_class_small == false) { // tu zmieniamy wysokosc - gdy strona zjedzie 50px w dol
+     $('.main-navigation').addClass('small'); // nawigacja otrzyma klase small
+     $('.logo').addClass('small'); // nawigacja otrzyma klase small
+     $('#nav').addClass('small'); // nawigacja otrzyma klase small
+    }
+    else {
+     $('.main-navigation').removeClass('small'); // w przeciwnym wypadku usuwamy klase small
+     $('.logo').removeClass('small'); // w przeciwnym wypadku usuwamy klase small
+     $('#nav').removeClass('small'); // w przeciwnym wypadku usuwamy klase small
+    }
+ });
+;
+//$(document).ready(function(){
+  //$("#btn2").click(function(){
+   // $(".blackphone-about").hide();
+ // });
+ // $("#openbtn1").click(function(){
+    //$(".blackphone-about").show();
+  //});
+ //});
+ 
